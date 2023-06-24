47da4c41f87e5efd573b59b44b1a78e9ccf00630
fmora122 <pfmoras@gmail.com>
2023-06-24 14:56:32 -0600

    Creating Project

---
+++ SC202-Group2-Project-Main/src/sc202/group2/project/main/SC202Group2ProjectMain.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sc202.group2.project.main;
+
+/**
+ *
+ * @author fmora
+ */
+public class SC202Group2ProjectMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
