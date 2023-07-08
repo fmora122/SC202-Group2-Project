90958c91b8da2517084b28ecfe80de3a286ce9e4
fmora122 <pfmoras@gmail.com>
2023-07-08 14:48:24 -0600

    Start 7/8/2023

--- SC202-Group2-Project-Main/src/sc202/group2/project/main/SC202Group2ProjectMain.java
+++ SC202-Group2-Project-Main/src/sc202/group2/project/main/SC202Group2ProjectMain.java
@@ -6,30 +6,9 @@ public class SC202Group2ProjectMain {
 
     
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Fabricio");
-=======
->>>>>>> d7cf75b2b382839cd4cedd41f435595f184d975f
-        System.out.println("Chris");
-        System.out.println("Alejhandro");
-        System.out.println("Fabricio");
-        System.out.println("Chris");
-        System.out.println("Fabricio's last test");
-<<<<<<< HEAD
-        System.out.println("Alejhandro");
-<<<<<<< HEAD
-        System.out.println("stress test");
-=======
-        System.out.println("double push");
->>>>>>> 2fb768676fc0706b48fd5ea97c64a74c43aa3ee3
-
-        // TODO code application logic here
-
-=======
-        // TODO code application logic here
-        Fabricio!
-        Alejhandro
->>>>>>> d7cf75b2b382839cd4cedd41f435595f184d975f
+        
+        // Menu
+        
     } 
     
 }
