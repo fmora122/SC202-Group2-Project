7a3e907ec36117702a3169fcdda8439a1eda3b12
Alejrr2 <alejhandrorr@gmail.com>
2023-07-08 16:37:31 -0600

    Class

--- SC202-Group2-Project-Main/src/sc202/group2/project/main/Paddocks.java
+++ SC202-Group2-Project-Main/src/sc202/group2/project/main/Paddocks.java
@@ -9,5 +9,84 @@ package sc202.group2.project.main;
  * @author XPC
  */
 public class Paddocks {
+    private int paddocksId =0;
+    private String paddocksName = "";
+    private String paddocksStatus = "";
+    private int cattleEntryDate =0;
+    private int cattleExitDate =0;
+    private int ammountCattle =0;
+    private String waterCattle = "";
+    private String terrainType ="";
+
+    public int getPaddocksId() {
+        return paddocksId;
+    }
+
+    public String getPaddocksName() {
+        return paddocksName;
+    }
+
+    public String getPaddocksStatus() {
+        return paddocksStatus;
+    }
+
+    public int getCattleEntryDate() {
+        return cattleEntryDate;
+    }
+
+    public int getCattleExitDate() {
+        return cattleExitDate;
+    }
+
+    public int getAmmountCattle() {
+        return ammountCattle;
+    }
+
+    public String getWaterCattle() {
+        return waterCattle;
+    }
+
+    public String getTerrainType() {
+        return terrainType;
+    }
+
+    public void setPaddocksId(int paddocksId) {
+        this.paddocksId = paddocksId;
+    }
+
+    public void setPaddocksName(String paddocksName) {
+        this.paddocksName = paddocksName;
+    }
+
+    public void setPaddocksStatus(String paddocksStatus) {
+        this.paddocksStatus = paddocksStatus;
+    }
+
+    public void setCattleEntryDate(int cattleEntryDate) {
+        this.cattleEntryDate = cattleEntryDate;
+    }
+
+    public void setCattleExitDate(int cattleExitDate) {
+        this.cattleExitDate = cattleExitDate;
+    }
+
+    public void setAmmountCattle(int ammountCattle) {
+        this.ammountCattle = ammountCattle;
+    }
+
+    public void setWaterCattle(String waterCattle) {
+        this.waterCattle = waterCattle;
+    }
+
+    public void setTerrainType(String terrainType) {
+        this.terrainType = terrainType;
+    }
+    
+    
+    
+    
+    
+    
+
     
 }
