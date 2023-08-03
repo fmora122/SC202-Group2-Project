62b2a5a56d035e3992bffc4a7867c2e4d38c6e40
kryz97 <cbarquero40796@ufide.ac.cr>
2023-08-02 22:31:26 -0600

    save

---
+++ SC202-Group2-Project-Main/src/sc202/group2/project/main/saveInfo.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sc202.group2.project.main;
+
+/**
+ *
+ * @author chris
+ */
+public class saveInfo {
+    
+    public class savePaddock {
+   
+    
+    }
+     
+    
+    public class saveEstate{
+        
+    
+    }
+    
+    public class saveCattle{
+        
+    
+    }
+    
+}
