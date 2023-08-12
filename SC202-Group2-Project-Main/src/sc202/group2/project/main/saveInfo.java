7ff1e5ca52a5508f1c0569635e5e35f0d5aa6a32
fmora122 <pfmoras@gmail.com>
2023-08-11 19:51:41 -0600

    changes to find files in local dir

--- SC202-Group2-Project-Main/src/sc202/group2/project/main/saveInfo.java
+++ SC202-Group2-Project-Main/src/sc202/group2/project/main/saveInfo.java
@@ -19,8 +19,10 @@ public class saveInfo {
     public static class savePaddock {
         public static void savePaddockInfo(int paddocksId, String paddocksName, String paddocksStatus, String cattleEntryDate, String cattleExitDate, String ammountCattle, String waterCattle, String terrainType) {
             //Ruta del archivo
-            String filePath = ".\\SC202-Group2-Project\\SC202-Group2-Project-Main\\src\\sc202\\group2\\project\\Paddock_Info.txt"; 
-
+            String rootDir = System.getProperty("user.dir");
+            String fileName = "Paddock_Info.txt";
+            String filePath = rootDir + "/" + fileName;
+        
             try {
                 // Crear o abrir el archivo para escritura
                 FileWriter fileWriter = new FileWriter(filePath, true); // El true permite añadir al archivo existente
@@ -55,8 +57,9 @@ public class saveInfo {
     public static class saveEstate {
         public static void saveEstateInfo(String estateName, String estateLocation, String estateManagerName, String estateManagerId, String estateManagerPhone, String estateNumberPaddocks, String estateSize, String estateTotalCattle) {
             //Ruta del archivo
-            String filePath = ".\\SC202-Group2-Project\\SC202-Group2-Project-Main\\src\\sc202\\group2\\project\\Estate_Info.txt"; 
-
+            String rootDir = System.getProperty("user.dir");
+            String fileName = "Estate_Info.txt";
+            String filePath = rootDir + "/" + fileName;
             try {
                 // Crear o abrir el archivo para escritura
                 FileWriter fileWriter = new FileWriter(filePath, true); // El true permite añadir al archivo existente
@@ -89,8 +92,11 @@ public class saveInfo {
     }
 
     public static void saveCattle(int cattleID, String cattleName, int cattleBirthdate, double cattleLastWeight, String cattleSexString, String cattleBreed) {
-        String filePath = ".\\SC202-Group2-Project\\SC202-Group2-Project-Main\\src\\sc202\\group2\\project\\Cattle_Info"; 
-
+        //String filePath = "/home/fmora/NetBeansProjects/SC202-Group2-Project/SC202-Group2-Project-Main/Cattle_Info"; 
+        String rootDir = System.getProperty("user.dir");
+        String fileName = "Cattle_Info";
+        String filePath = rootDir + "/" + fileName;
+        
         try {
 
             if (CattleIDUsed(cattleID)) {
@@ -124,8 +130,11 @@ public class saveInfo {
     }
 
     private static boolean CattleIDUsed(int cattleID) throws IOException {
-        String filePath = ".\\SC202-Group2-Project\\SC202-Group2-Project-Main\\src\\sc202\\group2\\project\\Cattle_Info"; //
-
+        //String filePath = "/home/fmora/NetBeansProjects/SC202-Group2-Project/SC202-Group2-Project-Main/Cattle_Info"; //
+        String rootDir = System.getProperty("user.dir");
+        String fileName = "Cattle_Info";
+        String filePath = rootDir + "/" + fileName;
+        
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
